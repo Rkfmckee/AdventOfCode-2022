232cffd470d7bbf2b82db9c484b6e18258497f13
Ryan McKeever <rkfmckee@gmail.com>
2022-12-01 13:34:14 +0000

    Initial commit

---
+++ Day1/Program.cs
@@ -0,0 +1,32 @@
+﻿var inputFilepath = "C:\\Dev\\AdventOfCode\\InputFiles\\Day1.txt";
+var reader = new StreamReader(inputFilepath);
+var nextLine = null as string;
+
+var calorieCount = new List<int>();
+var elfNum = 0;
+
+while((nextLine = reader.ReadLine()) != null)
+{
+    if (string.IsNullOrWhiteSpace(nextLine))
+    {
+        elfNum++;
+        continue;
+    }
+
+    if (calorieCount.ElementAtOrDefault(elfNum) == 0)
+    {
+        calorieCount.Insert(elfNum, 0);
+    }
+
+    var caloriesForElf = calorieCount[elfNum];
+    caloriesForElf += int.Parse(nextLine);
+    calorieCount.Insert(elfNum, caloriesForElf);
+}
+
+foreach (var calories in calorieCount)
+{
+    Console.WriteLine($"{calorieCount.IndexOf(calories)}: {calories}");
+}
+
+var max = calorieCount.Max();
+Console.WriteLine($"Elf with most calories: {calorieCount.IndexOf(max)}, Calories: {max}.");
